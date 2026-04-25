3857d3bc65ab28f15143568fc5b53b1bc6c92817
Conor Wallace <conorw8@gmail.com>
2026-04-25 14:18:03 -0500

    Fixed cli evaluation

--- src/bayes_tom/cli.py
+++ src/bayes_tom/cli.py
@@ -1,7 +1,35 @@
 import click
+from pathlib import Path
+
+
+def _load_config(path):
+    if path.suffix == ".yaml":
+        import yaml
+
+        with open(path, "r") as f:
+            config_dict = yaml.safe_load(f)
+    else:
+        raise NotImplementedError(f"Unable to read config type: {path.suffix}")
+
+    return config_dict
 
 
 @click.group
 @click.version_option()
 def main():
     pass
+
+
+@main.command()
+@click.argument("config")
+def evaluate(config):
+    """
+    Submit a job
+    """
+    from bayes_tom.evaluation import run_partner_evaluation
+
+    config_path = Path(config)
+
+    config_dict = _load_config(config_path)
+
+    run_partner_evaluation(config_dict)
---
+++ src/bayes_tom/evaluation/__init__.py
@@ -0,0 +1,5 @@
+from .partner_evaluator import run_partner_evaluation
+
+__all__ = [
+    "run_partner_evaluation",
+]
