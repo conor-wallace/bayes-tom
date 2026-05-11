cfcca133fa09c754ef8b55e76386b78a2df976fe
conor-wallace <conorw8@gmail.com>
2026-05-10 20:42:57 -0500

    Added new population interface

--- src/bayes_tom/agents/oracle_agent.py
+++ src/bayes_tom/agents/oracle_agent.py
@@ -9,15 +9,15 @@ class OracleAgent:
         self.population = population
 
     def init_hstate(self, batch_size=1, aux_info=None):
-        return self.population.policy_cls.init_hstate(batch_size, aux_info=aux_info)
+        return self.population.init_hstate(batch_size, aux_info=aux_info)
 
     def reset(self):
         pass
 
-    def get_action(self, params, partner_indices, obs, done, avail_actions, hstate, rng,
+    def get_action(self, partner_indices, obs, done, avail_actions, hstate, rng,
                    aux_obs=None, env_state=None, test_mode=False, trace=None):
 
         self.pred_partner_idx = partner_indices
 
-        return self.population.get_actions(params, partner_indices, obs, done, avail_actions,
+        return self.population.get_actions(partner_indices, obs, done, avail_actions,
                                            hstate, rng, env_state, aux_obs, test_mode=True)
--- src/bayes_tom/agents/random_agent.py
+++ src/bayes_tom/agents/random_agent.py
@@ -14,13 +14,16 @@ class RandomAgent:
         self.method = "random"
         self.population = population
 
+    # def init_hstate(self, batch_size=1, aux_info=None):
+    #     return self.population.policy_cls.init_hstate(1, aux_info={"agent_id": 0})
+
     def init_hstate(self, batch_size=1, aux_info=None):
-        return self.population.policy_cls.init_hstate(1, aux_info={"agent_id": 0})
+        return self.population.init_hstate(batch_size, aux_info=aux_info)
 
     def reset(self):
         pass
 
-    def get_action(self, params, partner_indices, obs, done, avail_actions, hstate, rng,
+    def get_action(self, partner_indices, obs, done, avail_actions, hstate, rng,
                    aux_obs=None, env_state=None, test_mode=False, trace=None):
 
         random_policy_idx = random.randint(0, self.population.pop_size-1)
@@ -28,5 +31,5 @@ class RandomAgent:
 
         self.pred_partner_idx = random_policy_idx
 
-        return self.population.get_actions(params, random_policy_idx, obs, done, avail_actions,
+        return self.population.get_actions(random_policy_idx, obs, done, avail_actions,
                                            hstate, rng, env_state, aux_obs, test_mode=True)
